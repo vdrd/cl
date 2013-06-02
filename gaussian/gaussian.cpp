9ff36b2a0702985610a492dbd1c0cdb08b3a5161
Ravishekhar Banger <ravibanger@gmail.com>
2013-06-02 17:17:44 +0530

    Adding Gaussian filter

---
+++ gaussian/gaussian.cpp
@@ -0,0 +1,248 @@
+#pragma	once
+#include "gaussian.h"
+#include "gaussian_kernel.h"
+#include <math.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+ImageFilter::ImageFilter(string &_filename)
+{
+
+	cout <<   "\n/*********************************************/ " << "\n";
+	cout <<	    "     Image Filter Base class                  "   << "\n";
+	cout <<	    "/*********************************************/ " << "\n\n";
+    host_image = NULL;
+    filename = _filename;
+    load_bmp_image();
+    setup_filter();    
+}
+
+void ImageFilter::setup_filter( )
+{
+    float lFilter[WINDOW_SIZE*WINDOW_SIZE] = {  1.f/16,  2.f/16,  1.f/16, 
+                                                2.f/16,  4.f/16,  2.f/16,
+                                                1.f/16,  2.f/16,  1.f/16  };
+    memcpy(filter, lFilter, WINDOW_SIZE*WINDOW_SIZE*sizeof(float));
+}
+
+void ImageFilter::load_bmp_image( )
+{
+    ReadBMPGrayscaleImageFloat(filename, &image);
+}
+
+void ImageFilter::write_bmp_image( )
+{
+    std::string filteredFileName("filtered.bmp") ;
+    WriteBMPGrayscaleImageFloat(filteredFileName, &image, GPU_output); 
+}
+
+void ImageFilter::read_GPU_filtered_image()
+{
+    size_t origin[3];
+    size_t region[3];
+    cl_int status = 0;
+	origin[0] = origin[1] = origin[2] = 0;
+	region[0] = image->width; region[1] = image->height; region[2] = 1;
+    status = clEnqueueReadImage(commandQueue, ocl_filtered_image, CL_TRUE, origin, region, 0, 0, GPU_output, 0, NULL, NULL);
+    LOG_OCL_ERROR(status, "Error # clEnqueueReadImage" );
+}
+
+void ImageFilter::load_GPU_raw_image()
+{
+    size_t origin[3];
+    size_t region[3];
+    cl_int status = 0;
+	origin[0] = origin[1] = origin[2] = 0;
+	region[0] = image->width; region[1] = image->height; region[2] = 1;
+    status = clEnqueueWriteImage(commandQueue, ocl_raw, CL_TRUE, origin, region, 0, 0, image->pixels, 0, NULL, NULL);
+    LOG_OCL_ERROR(status, "Error # clEnqueueReadImage" );
+}
+
+void ImageFilter::print_GPU_Timer()
+{
+	printf("GPU execution time is.......... %lf (ms)\n", 1000*timer_GPU.GetElapsedTime());
+}
+
+
+
+void ImageFilter::cleanup()
+{
+	free(host_image);
+    ReleaseBMPImage(&image);
+	cout << "Cleaned up!\n";
+}
+
+
+void ImageFilter::init_GPU_OpenCL( )
+{
+	//Allocate GPU output image memory
+	GPU_output = NULL; 
+    GPU_output = (float*) calloc(1, image->height*image->width*sizeof(float) );
+
+    setupOCLPlatform();
+    setupOCLProgram();
+    setupOCLkernels();
+    setupOCLbuffers();
+
+	gwsize[0] = image->width;
+	gwsize[1] = image->height;
+	lwsize[0] = lwsize[1] = 16;
+}
+
+void ImageFilter::setupOCLPlatform()
+{
+    cl_int status;
+	//Setup the OpenCL Platform, 
+	//Get the first available platform. Use it as the default platform
+    status = clGetPlatformIDs(1, &platform, NULL);
+    LOG_OCL_ERROR(status, "Error # clGetPlatformIDs" );
+
+	//Get the first available device 
+    status = clGetDeviceIDs (platform, deviceType, 1, &device, NULL);
+    LOG_OCL_ERROR(status, "Error # clGetDeviceIDs" );
+	
+	//Create an execution context for the selected platform and device. 
+    cl_context_properties cps[3] = 
+    {
+        CL_CONTEXT_PLATFORM,
+        (cl_context_properties)platform,
+        0
+    };
+    context = clCreateContextFromType(
+        cps,
+        deviceType,
+        NULL,
+        NULL,
+        &status);
+    LOG_OCL_ERROR(status, "Error # clCreateContextFromType" );
+
+    // Create command queue
+    commandQueue = clCreateCommandQueue(context,
+                                        device,
+                                        0,
+                                        &status);
+    LOG_OCL_ERROR(status, "Error # clCreateCommandQueue" );
+}
+
+cl_int ImageFilter::setupOCLProgram()
+{
+    cl_int status;
+    program = clCreateProgramWithSource(context, 1,
+                (const char **)&gaussian_kernel, NULL, &status);
+    LOG_OCL_ERROR(status, "clCreateProgramWithSource Failed" );
+
+    // Build the program
+    status = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
+    LOG_OCL_ERROR(status, "clBuildProgram Failed" );
+    if(status != CL_SUCCESS)
+    {
+        if(status == CL_BUILD_PROGRAM_FAILURE)
+            LOG_OCL_COMPILER_ERROR(program, device);
+        LOG_OCL_ERROR(status, "clBuildProgram Failed" );
+    }
+    return status;
+
+}
+
+cl_int ImageFilter::setupOCLkernels()
+{
+    cl_int status;
+    // Create the OpenCL kernel
+    gd_kernel = clCreateKernel(program, "gaussian_filter_kernel", &status);
+    LOG_OCL_ERROR(status, "clCreateKernel Failed" );
+
+    return status;
+}
+
+cl_int ImageFilter::setupOCLbuffers()
+{
+    cl_int status;
+	//Intermediate reusable cl buffers
+	cl_image_format image_format;
+    cl_image_desc image_desc;
+	image_format.image_channel_data_type = CL_FLOAT;
+	image_format.image_channel_order = CL_R;
+	
+	image_desc.image_type = CL_MEM_OBJECT_IMAGE2D;
+	image_desc.image_width = image->width;
+	image_desc.image_height = image->height;
+	image_desc.image_depth = 1;
+	image_desc.image_array_size = 1;
+    //Note when the host_ptr is NULL row_pitch and slice_pith should be set to 0;
+    //Otherwise you will get a CL_INVALID_IMAGE_DESCRIPTOR error
+	image_desc.image_row_pitch = 0; 
+	image_desc.image_slice_pitch = 0;
+	image_desc.num_mip_levels = 0;
+	image_desc.num_samples = 0;
+	image_desc.buffer= NULL;
+    ocl_raw = clCreateImage(
+        context,
+        CL_MEM_READ_ONLY,
+        &image_format,
+        &image_desc,
+		NULL, 
+        &status); 
+    LOG_OCL_ERROR(status, "clCreateImage Failed" );
+
+    //Note when the host_ptr is NULL row_pitch and slice_pith should be set to 0;
+    //Otherwise you will get a CL_INVALID_IMAGE_DESCRIPTOR error
+	image_desc.image_row_pitch = 0; 
+	image_desc.image_slice_pitch = 0;
+
+    ocl_filtered_image = clCreateImage(
+        context,
+        CL_MEM_WRITE_ONLY,
+        &image_format,
+        &image_desc,
+		NULL, 
+        &status); 
+    LOG_OCL_ERROR(status, "clCreateImage Failed" );
+
+    ocl_filter = clCreateBuffer(
+        context,
+        CL_MEM_READ_WRITE|CL_MEM_USE_HOST_PTR,
+        WINDOW_SIZE*WINDOW_SIZE*sizeof(float),
+		filter, 
+        &status); 
+    LOG_OCL_ERROR(status, "clCreateBuffer Failed" );
+
+	//Create OpenCL device output buffer
+    return status;
+}
+
+void ImageFilter::run_GPU()
+{
+	load_GPU_raw_image();
+	run_gaussian_filter_kernel();
+	read_GPU_filtered_image();
+}
+
+void ImageFilter::run_gaussian_filter_kernel()
+{
+	cl_event	wlist[2];
+    cl_int status;
+    
+    int windowSize = WINDOW_SIZE;
+    status = clSetKernelArg(gd_kernel, 0, sizeof(cl_mem), (void*)&ocl_raw); 
+    status = clSetKernelArg(gd_kernel, 1, sizeof(cl_mem), (void*)&ocl_filtered_image);
+    status = clSetKernelArg(gd_kernel, 2, sizeof(cl_mem), (void*)&ocl_filter);
+    status = clSetKernelArg(gd_kernel, 3, sizeof(int), (void*)&windowSize); 
+    status = clEnqueueNDRangeKernel(
+                        commandQueue,
+                        gd_kernel,
+                        2,
+                        NULL,
+                        gwsize,
+                        lwsize,
+                        0,
+                        NULL,
+                        &wlist[0]);
+    LOG_OCL_ERROR(status, "clEnqueueNDRangeKernel Failed" );
+    clWaitForEvents(1, &wlist[0]);
+
+}
+
+
---
+++ gaussian/gaussian_kernel.h
@@ -0,0 +1,30 @@
+#ifndef OCL_GAUSSIAN_KERNEL_H
+#define OCL_GAUSSIAN_KERNEL_H
+static const char *gaussian_kernel =
+"                                                                                                      \n"
+"__constant sampler_t mysampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE;             \n"
+"                                                                                                      \n"
+"__kernel void gaussian_filter_kernel(__read_only image2d_t iimage, __write_only image2d_t oimage,     \n"
+"                                         __global float *filter, int windowSize)                      \n"
+"{                                                                                                     \n"
+"    unsigned int x = get_global_id(0);                                                                \n"
+"    unsigned int y = get_global_id(1);                                                                \n"
+"    int halfWindow = windowSize/2;                                                                    \n"
+"    float4 pixelValue;                                                                                \n"
+"    float4 computedFilter=0.0f;                                                                       \n"
+"    int i, j, ifilter, jfilter;                                                                       \n"
+"                                                                                                      \n"
+"    for(i=-halfWindow, ifilter=0; i<=halfWindow; i++, ifilter++){                                     \n"
+"       for(j=-halfWindow, jfilter=0; j<=halfWindow; j++, jfilter++){                                  \n"
+"           pixelValue = read_imagef(iimage, mysampler, (int2)(x+i, y+j));                             \n"
+"           computedFilter += filter[ifilter*windowSize+jfilter]*pixelValue;                           \n"
+"       }                                                                                              \n"
+"    }                                                                                                 \n"
+"                                                                                                      \n"
+"                                                                                                      \n"
+"    write_imagef(oimage, (int2)(x, y), computedFilter);                                               \n"
+"}                                                                                                     \n"
+"                                                                                                      \n"
+"                                                                                                      \n";
+
+#endif
