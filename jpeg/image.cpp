309acea3cef585c3f390f36c490573f6e9c6a09f
ravibanger <ravishekhar.banger@amd.com>
2013-12-01 17:07:29 +0530

    working jpeg deleted unnecessary files moved code to common file.

--- jpeg/image.cpp
+++ jpeg/image.cpp
@@ -241,7 +241,6 @@ void image::decodeFrame(unsigned char SOFmarker)
             processTablenMisc(nextMarker);
             nextMarker = getNextMarker();
         }
-
         decodeScan();
 }
 
@@ -552,4 +551,47 @@ void image::decodeScan()
     return;
 }
 
+
+void printInfo(int condition, const char *fmt_string, ...)  
+{
+#ifdef _printInfo_
+        if(condition)
+        {
+                va_list arg;
+                va_start(arg,fmt_string);
+#ifdef WIN32
+                vprintf_s(fmt_string,arg); 
+#else
+                vprintf(fmt_string,arg); 
 #endif
+                va_end(arg);
+        }
+#endif 
+}
+
+void printError(int condition, const char *fmt_string, ...)
+{
+#ifdef _printError_
+        if(condition)
+        {
+                va_list arg;
+                va_start(arg,fmt_string);
+#ifdef WIN32
+                vprintf_s(fmt_string,arg); 
+#else
+                vprintf(fmt_string,arg); 
+#endif
+                va_end(arg);
+                getchar();
+                exit(-1);
+        }
+#endif
+}
+
+
+
+
+
+#endif
+
+
--- jpeg/image.h
+++ jpeg/image.h
@@ -2,10 +2,10 @@
 #define IMAGE_H
 
 //#include <io.h>
-#include "debug.h"
-#include "markers.h"
-#include "SOF0.h"
-#include "APP0.h"
+//#include "debug.h"
+//#include "markers.h"
+//#include "SOF0.h"
+#include "jpeg_marker.h"
 #include "Framewave.h"
 #define ENABLE_FRAMEWAVE 1
 #ifdef __APPLE__
@@ -15,6 +15,103 @@
 #endif
 #define OCL_SUCCESS 0
 #define OCL_FAILURE -1
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+
+#define _printInfo_ 
+void printInfo(int condition, const char *fmt_string, ...);
+void printError(int condition, const char *fmt_string, ...);
+
+typedef enum
+    {
+    // non-differential Huffman coding
+    SOF0  = 0xC0, // Baseline DCT
+    SOF1  = 0xC1, // Extended sequential DCT
+    SOF2  = 0xC2, // Progressive DCT
+    SOF3  = 0xC3, // Lossless(sequential)
+    
+    // differential Huffman coding
+    SOF5  = 0xC5, // Differential sequential DCT 
+    SOF6  = 0xC6, // Differential Progressive DCT
+    SOF7  = 0xC7, // Differential lossless(sequential)
+           
+    // non-differential arithematic coding
+    JPG   = 0xC8, // Reserved for JPEG extensions
+    SOF9  = 0xC9, // Extended sequential DCT
+    SOFA  = 0xCA, // Progressive DCT
+    SOFB  = 0xCB, // Lossless(sequential)
+    
+    // differential arithematic coding
+    SOFD  = 0xCD, // Differntial sequential DCT
+    SOFE  = 0xCE, // Differential progressive DCT
+    SOFF  = 0xCF, // Differential lossless(sequential)
+               
+    // Huffman table specifications
+    DHT   = 0xC4, // Define Huffman Table
+    
+    // Arithematic coding conditioning specification
+    DAC   = 0xCC, // Define Arithematic coding conditioning(s)
+    
+    // Restart Interval Termination
+    RST0  = 0xD0, // restart with modulo 8 count 'm'
+    RST1  = 0xD1,
+    RST2  = 0xD2,
+    RST3  = 0xD3,
+    RST4  = 0xD4,
+    RST5  = 0xD5,
+    RST6  = 0xD6,
+    RST7  = 0xD7,
+    
+    // Other markers
+    SOI   = 0xD8, // Start Of Image
+    EOI   = 0xD9, // End Of Image
+    SOS   = 0xDA, // Start of Scan
+    DQT   = 0xDB, // Define Quantisation Table(s)
+    DNL   = 0xDC, // Define Number of Lines
+    DRI   = 0xDD, // Define Restart Interval
+    DHP   = 0xDE, // Define Hierarchical Progression
+    EXP   = 0xDF, // Expand Reference Component(s)
+
+    APP0  = 0xE0, // Reserved for Application segments JFIF
+    APP1  = 0xE1,
+    APP2  = 0xE2,
+    APP3  = 0xE3,
+    APP4  = 0xE4,
+    APP5  = 0xE5,
+    APP6  = 0xE6,
+    APP7  = 0xE7,
+    APP8  = 0xE8,
+    APP9  = 0xE9,
+    APPA  = 0xEA,
+    APPB  = 0xEB,
+    APPC  = 0xEC,
+    APPD  = 0xED,
+    APPE  = 0xEE,
+
+    JPG0  = 0xF0,// Reserved for JPEG Extensions
+    JPG1  = 0xF1,
+    JPG2  = 0xF2,
+    JPG3  = 0xF3,
+    JPG4  = 0xF4,
+    JPG5  = 0xF5,
+    JPG6  = 0xF6,
+    JPG7  = 0xF7,
+    JPG8  = 0xF8,
+    JPG9  = 0xF9,
+    JPGA  = 0xFA,
+    JPGB  = 0xFB,
+    JPGC  = 0xFC,
+    JPGD  = 0xFD,
+
+    COM   = 0xFE, // Comment
+              
+    // Reserved markers
+    TEM   = 0x01, // for Temporary private use in arithematic coding 
+    RES   = 0x02  // Reserved from 0x02 upto 0xBF
+    }MARKER;
+
 typedef struct
 {
     unsigned char ComponentId;
@@ -74,6 +171,9 @@ public:
     void decodeScan();
     /*Converts the YCbCr data to BGR and writes the BMP file*/
     void write(const char *fname, Fw8u *imageYCbCr);
+
+
 };
 
 #endif
+
